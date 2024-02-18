115997da4568702580b3433b1acc56a923f1cc71
celsonfernand0 <celsonfernando100@outlook.com>
2024-02-19 00:34:07 +0100

    ex poo_Pessoa

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,31 @@
 fun main() {
+    val pessoas: MutableList<Pessoa> = mutableListOf(
+        Pessoa("Celson Fernando Milombe Paixão", 18, 54.9, altura = 1.89),
+        Pessoa("João José Milombe", 22, 63.8, 1.67)
+    )
+
+    println("Vamos adicionar uma novo Usuario a sua lista")
+
+    println("Informe o nome")
+    val newName: String = readln()
+
+    println("Informe a idade")
+    val newOld: Int = readln().toInt()
+
+    println("Informe o peso")
+    val newweight: Double = readln().toDouble()
+
+    println("Informe a altura")
+    val newheigh: Double = readln().toDouble()
+
+    pessoas.add(Pessoa(newName, newOld, newweight, newheigh))
+
+    pessoas.reverse()
+
+    pessoas.forEach { pessoa ->
+        println(" ${pessoa.nome} está com ${pessoa.idade} anos de idade, pesa ${pessoa.peso} Kg e mede ${pessoa.altura} metros de altura ")
+
+    }
+
 
 }
---
+++ src/Pessoa.kt
@@ -0,0 +1,6 @@
+data class Pessoa(
+    val nome: String,
+    val idade: Int,
+    val peso: Double,
+    val altura: Double,
+)
